e501e3838d66580471140a754eabc893d66250d2
Shivankit Agarwal <shivankit1210@gmail.com>
2024-08-05 19:50:22 +0530

    Editing in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="w-full h-12 gap-96 p-4  items-center flex flex-row bg-blue-300 ">
-        <div className=" text-orange-300 bg-white p-1 w-full rounded-r-xl">
-          ShivOhm News
+      <div className="w-full h-12 p-4  items-center flex flex-row bg-blue-300 ">
+        <div className=" text-orange-300 w-36 text-sm md:text-lg bg-white p-1 md:w-36 rounded-r-xl whitespace-nowrap overflow-hidden ">
+          ShivOhm Articles
         </div>
         <div>
-          <ul className="flex flex-row gap-5 invisible text-slate-600 md:visible ">
+          <ul className=" flex-row gap-5  hidden text-slate-600 md:visible ">
             <li className="cursor-pointer">HOME</li>
             <li className="cursor-pointer">ABOUT US</li>
             <li className="cursor-pointer">CONTACT US</li>
             <li className="cursor-pointer">QUICK LINKS</li>
           </ul>
-          <div className="  visible md:invisibe">
+          <div className="  visible md:hidden">
             <span > &#x2630;</span>
           </div>
         </div>
@@ -24,3 +24,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+x
